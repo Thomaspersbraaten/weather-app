b7e876401a9f72c543cbae1d3cbe46167be258e6
Thomas Persbråten <83210455+Thomaspersbraaten@users.noreply.github.com>
2023-03-18 12:27:07 +0100

    O

--- src/components/api/locationForecast.tsx
+++ src/components/api/locationForecast.tsx
@@ -1,1 +1,4 @@
-export const locForecast: String = "https://crossorigin.mehttps://api.met.no/weatherapi/locationforecast/2.0/compact";
+// export const locForecast: String = "https://crossorigin.me//api.met.no/weatherapi/locationforecast/2.0/compact";
+const corsProxy = "https://cors-anywhere.herokuapp.com/";
+
+export const locForecast: String = corsProxy + "https://api.met.no/weatherapi/locationforecast/2.0/compact";
--- src/components/pages/CityDetail.tsx
+++ src/components/pages/CityDetail.tsx
@@ -6,26 +6,49 @@ import City from "../types/city";
 
 function CityDetail() {
   const { city } = useParams();
-  const [cityState, setCityState] = useState();
-  console.log(city);
+  const [cityState, setCityState] = useState<City>();
+  const [error, setError] = useState<Boolean>(false);
+  const [cityData, setCityData] = useState([]);
   useEffect(() => {
-    console.log(cities);
-    const currentCity = cities.filter((cit) => {
+    const currentCity = cities.find((cit: City) => {
       return cit.city === city;
     });
-    console.log(currentCity);
-    setCityState(currentCity);
-
-    async function getWeatherForCity() {
-      const weaterApi = locForecast + `?lat=${currentCity.lat}`;
-      try {
-      } catch (error) {
-        console.log(error);
-      }
+    if (currentCity) {
+      const lat = Number(currentCity.lat).toFixed(2);
+      const lng = Number(currentCity.lng).toFixed(2);
+      setCityState({ ...currentCity, lat, lng });
     }
   }, [city]);
 
-  return <div>CityDetail</div>;
+  useEffect(() => {
+    async function getCityData() {
+      if (cityState) {
+        const weatherApi = locForecast + `?lat=${cityState?.lat}&lon=${cityState?.lng}`;
+        try {
+          const response = await fetch(weatherApi);
+          const json = await response.json();
+          console.log(json.properties);
+          if (json.properties) {
+            setCityData(json.properties);
+          }
+        } catch (error) {
+          console.log(error);
+          setError(true);
+        }
+      }
+    }
+    getCityData();
+  }, [cityState]);
+
+  return (
+    <div className="p-10">
+      {error && <h1>GTFO</h1>}
+      <h1 className="text-5xl font-bold">{city}</h1>
+      <ul>{cityData && <li>s</li>}</ul>
+    </div>
+  );
 }
 
 export default CityDetail;
+
+// const [cityState, setCityState] = useState({});
